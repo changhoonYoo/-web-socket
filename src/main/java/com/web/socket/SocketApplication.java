55a9fcf69dd9098be53ac6d8e05cc5c8fc94aeb2
changhoonYoo <kuilcake@gmail.com>
2023-12-05 17:36:01 +0900

    웹소켓 공부즁

--- src/main/java/com/web/socket/SocketApplication.java
+++ src/main/java/com/web/socket/SocketApplication.java
@@ -2,6 +2,8 @@ package com.web.socket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @SpringBootApplication
 public class SocketApplication {
@@ -10,4 +12,8 @@ public class SocketApplication {
 		SpringApplication.run(SocketApplication.class, args);
 	}
 
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
 }
